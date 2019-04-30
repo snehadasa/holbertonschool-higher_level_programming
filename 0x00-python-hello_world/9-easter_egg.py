fa216b26a29313e5eff6d50618a50f5e904bc03a
snehadasa <sneha.dasasneha@gmail.com>
2019-04-29 21:18:37 -0700

    check for o/p

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-a = """
+print("""
 The Zen of Python, by Tim Peters\n
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -20,6 +20,4 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
-"""
-
-print(a)
+""")
