d90ee44a65b46ab73bed031b3969819fe066f96a
snehadasa <sneha.dasasneha@gmail.com>
2019-04-30 21:25:57 -0700

    convert to uppercase

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,9 +15,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 
 	new_node->n = number;
-	new_node->next = NULL;
-
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
