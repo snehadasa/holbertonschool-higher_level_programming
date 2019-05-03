2d211981ba9dc1b908a79ac362b721bfa81040cf
snehadasa <sneha.dasasneha@gmail.com>
2019-05-02 22:26:25 -0700

    typo

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.4
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
     for i in names:
-        if names[0:2] != '__':
+        if i[0:2] != '__':
             print(i)
