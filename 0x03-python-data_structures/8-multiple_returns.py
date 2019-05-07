2447d6410cb45b7e1a12324b9a009569062b8f17
snehadasa <sneha.dasasneha@gmail.com>
2019-05-07 16:21:17 -0700

    empty sentence fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        sentence[0] = None
+        sentence = None
     str_len = len(tuple(sentence))
     first = sentence[0]
     return (str_len, first)
