30f39b6b94b501a5d75663af92eb7a9e27ac9ebd
snehadasa <sneha.dasasneha@gmail.com>
2019-05-07 16:32:49 -0700

    empty sentence check

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
-    if length == 0:
+    if length != 0:
+        first = sentence[0]
+    else:
         first = None
     return (length, first)
