87e7b49abba7bb5d4b663fcdd9aec8ad7077e70b
snehadasa <sneha.dasasneha@gmail.com>
2019-05-07 12:23:30 -0700

    not in range

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     total_len = len(my_list)
     if idx < 0:
         return None
-    elif idx > total_len:
+    elif idx not in range(total_len):
         return None
     else:
         return my_list[idx]
