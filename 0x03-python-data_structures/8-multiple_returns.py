ad5c38c0902253190262e1b1da1873c7261b04eb
snehadasa <sneha.dasasneha@gmail.com>
2019-05-07 16:17:51 -0700

    return length and first char

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        sentence[0] = ""
+    str_len = len(tuple(sentence))
+    first = sentence[0]
+    return (str_len, first)
+    
