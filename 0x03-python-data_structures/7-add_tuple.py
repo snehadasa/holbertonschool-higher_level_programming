a32b78c484177ab61da7607fceb8643817cc03df
snehadasa <sneha.dasasneha@gmail.com>
2019-05-07 22:03:03 -0700

    tuple another solution

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a += (0, 0)
-    tuple_b += (0, 0)
-    add_max = tuple_a[0] + tuple_b[0]
-    add_min = tuple_a[1] + tuple_b[1]
-    return (add_max, add_min)
+    first = 0
+    second = 0
+    if len(tuple_a) > 0:
+        first += tuple_a[0];
+    if len(tuple_a) > 1:
+        second += tuple_a[1];
+    if len(tuple_b) > 0:
+        first += tuple_b[0];
+    if len(tuple_b) > 1:
+        second += tuple_b[1];
+    return (first, second);
