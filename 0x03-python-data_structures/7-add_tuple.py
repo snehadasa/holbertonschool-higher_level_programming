6247566b284207e9026e2151511075424be162d5
snehadasa <sneha.dasasneha@gmail.com>
2019-05-07 19:19:13 -0700

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a += [0, 0]
-    tuple_b += [0, 0]
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
     add_max = tuple_a[0] + tuple_b[0]
     add_min = tuple_a[1] + tuple_b[1]
     return (add_max, add_min)
