4253897330cf91af692747b3e09d128331c5c338
snehadasa <sneha.dasasneha@gmail.com>
2019-05-09 14:19:10 -0700

    return

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    squ = [[pow(x, 2) for x in col] for col in matrix]
-    return squ
+    return [[pow(x, 2) for x in col] for col in matrix]
