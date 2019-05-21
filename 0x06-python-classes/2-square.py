00544e417f7a65279e998bbf1c306b76542ecb6e
snehadasa <sneha.dasasneha@gmail.com>
2019-05-21 15:43:45 -0700

    pep8 changes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square():
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
