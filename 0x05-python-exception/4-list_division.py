23d3d3c18ea46cf17b8df100d88f2edefe38b8a0
snehadasa <sneha.dasasneha@gmail.com>
2019-05-20 17:38:28 -0700

    small excute errors

--- 0x05-python-exception/4-list_division.py
+++ 0x05-python-exception/4-list_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new = []
-    result = 0.0
+    result = 0
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
+            i += 1
         except TypeError:
             print("wrong type")
             result = 0
