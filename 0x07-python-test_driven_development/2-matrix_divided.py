d4963cab906d703ca23949656007d2bfb7890797
snehadasa <sneha.dasasneha@gmail.com>
2019-05-24 22:56:32 -0700

    check if rows in matrix are equal

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,6 +13,8 @@ def matrix_divided(matrix, div):
         if not isinstance(i, list):
             raise TypeError("matrix must be a matrix"
                             "(list of lists) of integers/floats")
+        if (len(i) != len(matrix[0])):
+            raise TypeError("Each row of the matrix must have the same size")
         for j in i:
             if not isinstance(j, int) and not isinstance(j, float):
                 raise TypeError("matrix must be a matrix"
