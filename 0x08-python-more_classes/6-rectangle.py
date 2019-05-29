753043001de4eedc17e5f76d835bec3ec45420d4
snehadasa <sneha.dasasneha@gmail.com>
2019-05-28 21:46:01 -0700

    documentation

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Rectangle class that defines a rectangle
+with variables width and height"""
+
 class Rectangle():
+    """contains area, perimeters, __str__ & __repr__(object),
+     initialising public variables, getters & setters.
+     initialize private variables."""
     number_of_instances = 0
 
     def __init__(self, width = 0, height = 0):
