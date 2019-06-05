25fa45ed5dcb385fad4ce036b84780f89a4e3af4
snehadasa <sneha.dasasneha@gmail.com>
2019-06-05 08:48:42 -0700

    look for attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""module for looking an obj"""
+
+
 def lookup(obj):
+    """
+        lookup - looks for available attributes and returns them.
+        @obj: object
+    """
     return dir(obj)
