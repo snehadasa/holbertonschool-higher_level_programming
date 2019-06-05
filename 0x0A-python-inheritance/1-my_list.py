3fcfb34b957665ff6610f455ffebc672aeb2448f
snehadasa <sneha.dasasneha@gmail.com>
2019-06-05 10:10:56 -0700

    pep8 fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,6 @@ class MyList(list):
     """module that inherits from list
     subclass(MyList) of super class(list)"""
 
-
     def __init__(self):
         """initialisation"""
         list.__init__(self)
