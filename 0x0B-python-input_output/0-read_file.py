6f2f03023e7ca356e3155bb3ac17edeed4a55715
snehadasa <sneha.dasasneha@gmail.com>
2019-06-06 12:45:46 -0700

    converting str to json object

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """module to read and print the contents to stdout"""
+
+
 def read_file(filename=""):
     with open(filename, encoding='utf-8') as myFile:
         s = myFile.read()
