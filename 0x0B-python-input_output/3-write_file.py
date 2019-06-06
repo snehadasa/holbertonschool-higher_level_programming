bc4d807d3e89168c3f992ad0ae702ae7d1b8771a
snehadasa <sneha.dasasneha@gmail.com>
2019-06-06 11:27:40 -0700

    write and append

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module to write a string to a text file and return number of
+characters written"""
+
+
+def write_file(filename="", text=""):
+     with open(filename, mode = 'w', encoding='utf-8') as myFile:
+         s = myFile.write(text)
+         return s
---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module to write a append to a text file and return number of
+characters written"""
+
+
+def append_write(filename="", text=""):
+     with open(filename, mode = 'a', encoding='utf-8') as myFile:
+         s = myFile.write(text)
+         return s
