9426307ad3781228dc4b6f7ff1d0bb69c34f5e03
snehadasa <sneha.dasasneha@gmail.com>
2019-06-07 10:27:37 -0700

    pep8

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,9 +3,10 @@
     module that inherits from int
     This is an example of test overriden methods(__eq__(==), __ne__(!=))
 """
+
+
 class MyInt(int):
     """MyInt is a rebel which has '==' and '!=' inverted"""
-
 
     def __eq__(self, other):
         return super().__ne__(other)
