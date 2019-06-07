16f880f8600e6fead65ec1871a48307cd300613e
snehadasa <sneha.dasasneha@gmail.com>
2019-06-07 01:43:46 +0000

    if key found, return the appended dict

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -8,10 +8,11 @@ class Student():
 		self.last_name = last_name
 		self.age = age
 
-	def to_json(self):
-		if type(attrs) == list:
-			func = self.__dict__.copy()
-			for key in list(f.keys()):
-				if key not in attrs:
-del func[key]
-return 
+	def to_json(self, attrs=None):
+		if type(attrs) != list:
+			return self.__dict__
+		result = {}
+		for i in attrs:
+			if i in self.__dict__:
+				result[i] = self.__dict__[i]
+		return result
