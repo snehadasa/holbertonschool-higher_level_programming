7a5313363a3ea9ebc4987ee4e58f07076eadbcd3
snehadasa <sneha.dasasneha@gmail.com>
2019-06-12 17:28:21 -0700

    file not found error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -65,10 +65,13 @@ class Base():
     @classmethod
     def load_from_file(cls):
         res = []
-        with open(str(cls.__name__) + ".json", "r") as myFile:
-            if myFile is None:
+        try:
+            with open(str(cls.__name__) + ".json", "r") as myFile:
+                if myFile is None:
+                    return res
+                list_obj = cls.from_json_string(myFile.read())
+                for list_row in list_obj:
+                    res.append(cls.create(**list_row))
                 return res
-            list_obj = cls.from_json_string(myFile.read())
-            for list_row in list_obj:
-                res.append(cls.create(**list_row))
+        except FileNotFoundError:
             return res
