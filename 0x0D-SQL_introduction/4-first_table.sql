a87f7906bfc12fe5274652c4d2338eea4e4dc2f5
snehadasa <sneha.dasasneha@gmail.com>
2019-07-15 13:08:44 -0700

    task 7

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
---script to crestea a table in current database in MySQL server
---script to crestea a table in current database in MySQL server
+-- script to crestea a table in current database in MySQL server
+-- script to crestea a table in current database in MySQL server
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
---script to insert a new row in the table in MySQL server
---script to insert a new row in the table in MySQL server
+-- script to insert a new row in the table in MySQL server
+-- script to insert a new row in the table in MySQL server
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
