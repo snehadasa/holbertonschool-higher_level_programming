3922cecea3f4b607dd236ceefebea9f6a484e5c9
snehadasa <sneha.dasasneha@gmail.com>
2019-07-16 14:57:46 -0700

    task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,6 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_showd.id=tv_show_genres.show_id
+ON tv_shows.id=tv_show_genres.show_id
 WHERE genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genre.genre_is;
