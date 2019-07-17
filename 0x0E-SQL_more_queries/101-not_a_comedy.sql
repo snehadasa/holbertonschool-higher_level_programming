78a46511bbc625cc9a34c5b4aeebf30ec0f7b54c
snehadasa <sneha.dasasneha@gmail.com>
2019-07-16 20:24:23 -0700

    task 102

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,2 +1,15 @@
--- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
--- script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+--  script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+--  script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexte
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN
+(
+	SELECT tv_shows.title
+	FROM tv_shows
+	LEFT JOIN tv_show_genres
+	ON tv_shows.id=tv_show_genres.show_id
+	LEFT JOIN tv_genres
+	ON tv_show_genres.genre_id=tv_genres.id
+	WHERE tv_genres.name='Comedy'
+)
+ORDER BY tv_shows.title;
