df05daeb7b7fcfb5d52ad97d8f9fdc93265d0e90
snehadasa <sneha.dasasneha@gmail.com>
2019-07-16 20:38:11 -0700

    task 102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS "rating" FROM tv_shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) rating FROM tv_shows
 LEFT JOIN tv_show_ratings
-ON tv_show_ratings.show_id=tv_shows.id
+ON tv_shows.id=tv_show_ratings.show_id
 ORDER BY rating DESC;
