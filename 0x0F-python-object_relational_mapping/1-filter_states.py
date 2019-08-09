c245d6c47f347767b7ee93d15e88ff790e9edf4e
snehadasa <sneha.dasasneha@gmail.com>
2019-08-09 19:59:13 +0000

    indentation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""script that lists all states with a name starting with N
+   (upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sqlalchemy
 from sys import argv
 
 
-if __name__=='__main__':
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     states = cur.fetchall()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""script that takes in an argument and displays all values
+   in the states table of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 import sqlalchemy
@@ -10,7 +11,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'".format(argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
