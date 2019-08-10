e2b3e0ece37fdb816378cbc6cc662db1cd40956c
snehadasa <sneha.dasasneha@gmail.com>
2019-08-10 20:35:42 +0000

    indentation

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,11 +9,10 @@ from model_state import Base
 from model_state import State
 
 
-
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(argv[1], argv[2], argv[3]),
-                            pool_pre_ping=True)
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
     sq = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
