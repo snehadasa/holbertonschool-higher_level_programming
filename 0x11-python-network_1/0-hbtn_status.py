208841aa6d1a4f86217df82deaa10040c939a1d1
snehadasa <sneha.dasasneha@gmail.com>
2019-08-26 14:10:03 +0000

    initial commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        res = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
+    print("\t- utf8 content: {}".format(res.decode("utf-8")))
