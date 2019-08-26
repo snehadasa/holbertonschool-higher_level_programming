4aa8f37f1d77e744273936f5c7b325efe011f0c0
snehadasa <sneha.dasasneha@gmail.com>
2019-08-26 16:38:37 +0000

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and displays
+   the body of the response.
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    code = req.status_code
+    if code > 400:
+        print("Error code: {}".format(code))
+    else:
+        print(req.text)
