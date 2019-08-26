1b12af9274246fc2b53a158d696e07152ec1fc65
snehadasa <sneha.dasasneha@gmail.com>
2019-08-26 15:14:18 +0000

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status
+"""
+
+
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+    res = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
