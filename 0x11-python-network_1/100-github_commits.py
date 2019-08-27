bf28d94bd1c18db1074d3b654b986200643fed2f
snehadasa <sneha.dasasneha@gmail.com>
2019-08-27 16:40:15 +0000

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,10 +11,13 @@ if __name__ == "__main__":
     req = requests.get('https://api.github.com/repos/{}/{}/commits/'
                        .format(argv[2], argv[1])).json()
     c = 0
-    for commit in req:
+    for ar in req:
         if c == 10:
             break
-        name = commit.get("commit").get("author").get("name")
-        sha = commit.get("sha")
-        print("{}: {}".format(sha, name))
-        c += 1
+        try:
+            name = ar.get("commit").get("author").get("name")
+            sha = ar.get("sha")
+            print("{}: {}".format(sha, name))
+            c += 1
+        except AttributeError:
+            pass
