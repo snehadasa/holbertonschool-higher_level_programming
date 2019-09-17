ae7ac86db2c30a91d7d9d1c8d5d05dc04065981b
snehadasa <sneha.dasasneha@gmail.com>
2019-09-17 17:58:33 +0000

    script that prints the first argument passed to it:

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*if(process.argv[3] === undefined) {
+	console.log('No argument');
+} else {
+	console.log(process.argv[2]);
+}*/
+if(process.argv[2]) {
+	console.log(process.argv[2]);
+} else {
+	console.log('No argument');
+}
