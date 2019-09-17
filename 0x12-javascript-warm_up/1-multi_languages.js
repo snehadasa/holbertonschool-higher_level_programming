65591444df01b9b8e1d364f33812466274388997
snehadasa <sneha.dasasneha@gmail.com>
2019-09-17 17:02:02 +0000

    print multilines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myvar1 = 'C is fun';
+const myvar2 = 'Python is cool';
+const myvar3 = 'Javascript is amazing';
+
+console.log(myvar1.slice());
+console.log(myvar2.slice());
+console.log(myvar3.slice());
