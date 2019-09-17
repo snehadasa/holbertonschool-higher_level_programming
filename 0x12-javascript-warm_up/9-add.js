25eeab7a2a9d827e33a2695cc1c535e0441f79b0
snehadasa <sneha.dasasneha@gmail.com>
2019-09-17 21:11:22 +0000

    add two numbers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+function add (a, b) {
+  return (a + b);
+}
+console.log(add(a, b));
+/* if (process.argv.length < 4) {
+  console.log('NaN');
+} */
