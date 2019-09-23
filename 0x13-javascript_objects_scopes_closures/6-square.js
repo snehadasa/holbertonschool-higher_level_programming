3220da6cb753c0c7d67174f714d1b5c5f094e496
snehadasa <sneha.dasasneha@gmail.com>
2019-09-23 19:09:07 +0000

    add class square that inherits from rectangle using extend and require to call rectangle from another file and add print method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const SquareN = require('./5-square.js');
+module.exports = class Square extends SquareN {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log('c'.repeat(this.height));
+    }
+  }
+};
