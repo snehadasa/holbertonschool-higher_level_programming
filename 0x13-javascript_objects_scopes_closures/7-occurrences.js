4438df252658ffd1890f4bedda294776105ef64f
snehadasa <sneha.dasasneha@gmail.com>
2019-09-23 19:29:37 +0000

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,9 +3,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let number = 0;
   for (let i = 0; i < list.length; i++) {
-    if (searchElement == list[i]) {
+    if (searchElement === list[i]) {
       number++;
     }
   }
   return number;
-}
+};
