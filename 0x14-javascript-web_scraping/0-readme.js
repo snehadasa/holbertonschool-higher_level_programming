0655afe14d04b714f22483487d9c65df301606b7
snehadasa <sneha.dasasneha@gmail.com>
2019-09-24 21:13:39 +0000

    throw err

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,9 @@
 
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
