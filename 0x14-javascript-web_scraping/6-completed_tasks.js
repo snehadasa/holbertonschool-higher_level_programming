4110348c3bca13a9f79fb890600f2f656333ba18
snehadasa <sneha.dasasneha@gmail.com>
2019-09-24 20:28:01 +0000

    edit

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,7 +5,8 @@ request.get(process.argv[2], function (err, response, body) {
   if (err) throw err;
   const dic = {};
   const b = JSON.parse(body);
-  for (const t of b) {
+  for (let i in b) {
+    let t = b[i]
     if (t.completed) {
       if (t.userId in dic) {
         dic[t.userId] += 1;
