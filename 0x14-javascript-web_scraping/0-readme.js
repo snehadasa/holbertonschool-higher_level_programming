c0e6ddf8cb23cb1d5f40a535308b5637008b9554
snehadasa <sneha.dasasneha@gmail.com>
2019-09-24 20:04:02 +0000

    Revert "0-readme.js"
    This reverts commit 9f3aa68559e024535ff3cd1c26e76e5bbc0f21f8.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
-  } else {
-  console.log(data);
   }
+  console.log(data);
 });
