586f36fbd070a2cb6ae45bc49e801f6ad88beb0d
snehadasa <sneha.dasasneha@gmail.com>
2019-09-24 21:19:40 +0000

    check if api is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,9 @@ request.get(process.argv[2], function (err, response, body) {
   if (err) throw err;
   const starwars = JSON.parse(body).results.filter(count => {
     const num = count.characters.includes('https://swapi.co/api/people/18/');
-    return (num);
+    if (num) {
+      return (num);
+    }
   });
   const c = starwars.length;
   console.log(c);
