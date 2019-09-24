3d47cf952c66f9e20a6cefbf33877b76e1b45145
snehadasa <sneha.dasasneha@gmail.com>
2019-09-24 18:47:06 +0000

    return the count of the character present in the api/people with its id

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+request.get(process.argv[2], function (err, response, body) {
+  if (err) throw err;
+  const starwars = JSON.parse(body).results.filter(count => {
+    const num = count.characters.includes('https://swapi.co/api/people/18/');
+    return (num);
+  });
+  const c = starwars.length;
+  console.log(c);
+});
