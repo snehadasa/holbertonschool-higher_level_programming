2fd7c7343bdb50cd55f3f5a2a1ff6f65ccac4975
snehadasa <sneha.dasasneha@gmail.com>
2019-09-24 18:44:29 +0000

    semistandard fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get(process.argv[2], function(err, response) {
+request.get(process.argv[2], function (err, response) {
   if (err) {
     console.log(err);
   }
