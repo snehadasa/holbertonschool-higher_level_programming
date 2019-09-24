e98752c6f56caac8fd21e5be70268cb6b312ea7d
snehadasa <sneha.dasasneha@gmail.com>
2019-09-24 21:32:51 +0000

    added diff code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,12 +3,14 @@
 const request = require('request');
 request.get(process.argv[2], function (err, response, body) {
   if (err) throw err;
-  const starwars = JSON.parse(body).results.filter(count => {
-    const num = count.characters.includes('https://swapi.co/api/people/18/');
-    if (num) {
-      return (num);
+  const starwars = JSON.parse(body).results;
+  let c = 0;
+  for (let i = 0; i < starwars.length; i++) {
+    for (let j = 0; j < starwars[i].characters.length; j++) {
+      if (starwars[i].characters[j].includes('/18/')) {
+        c++;
+      }
     }
-  });
-  const c = starwars.length;
+  }
   console.log(c);
 });
