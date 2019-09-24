bcf98a7bcb1c0227795e7d29c274c559c8aa7a40
snehadasa <sneha.dasasneha@gmail.com>
2019-09-24 20:29:19 +0000

    semistandard fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,8 +5,8 @@ request.get(process.argv[2], function (err, response, body) {
   if (err) throw err;
   const dic = {};
   const b = JSON.parse(body);
-  for (let i in b) {
-    let t = b[i]
+  for (const i in b) {
+    const t = b[i];
     if (t.completed) {
       if (t.userId in dic) {
         dic[t.userId] += 1;
