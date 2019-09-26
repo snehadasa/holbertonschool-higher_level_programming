ced4a5cdc9ec4472c1b8fb0e05949d7dfc0d771f
snehadasa <sneha.dasasneha@gmail.com>
2019-09-26 22:52:25 +0000

    task 11 to add remove and clear list

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
+}); 
+
+$('DIV#remove_item').click(function () {
+  $('UL.my_list').children().last().remove();
+}); 
+
+$('DIV#clear_list').click(function () {
+  $('UL.my_list li').empty();
+}); 
